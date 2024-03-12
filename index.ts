6800079062e75716ea2db4f0cb9d81d0821df5c9
Danial Hanif <danialhanif@outlook.com>
2024-03-12 20:37:56 +0800

    feat(index.ts): feat: add passing sourceFiles option and expose onLoadCallback

--- index.ts
+++ index.ts
@@ -1,87 +1,158 @@
-import { readFileSync } from "node:fs";
-import type { BunPlugin } from "bun";
+import type { BunPlugin, OnLoadArgs, OnLoadResult, PluginBuilder } from "bun";
 
-function JSXScriptBundlerPlugin(config?: { verbose?: true }): BunPlugin {
-	const regexp = /<script .*((?=npm:|http:\/\/|https:\/\/).*?)(?=")/g;
+type Options = { verbose?: true; inline?: true };
+
+type ExtraArgs = Record<string, unknown> & {
+	/**
+	 * A map of source files to use instead.
+	 * Use this to pass previous transpiled code if any.
+	 * Otherwise, it will read the file from the file system.
+	 * `{ [filePath: string]: code }`
+	 */
+	sourceFiles?: { [filePath: string]: string };
+};
+
+const pluginName = "bun-plugin-jsx-script-bundle";
+
+const handleRemoteScripts = async (
+	build: PluginBuilder,
+	match: string,
+	groups: string[],
+	options?: Options,
+) => {
+	const [url, endTag] = groups;
+	if (options?.verbose) console.log(`${pluginName}: Downloading ${url}`);
+
+	const res = await fetch(url);
+	if (!res.ok) {
+		console.error(`${pluginName}: Failed to fetch ${url}`);
+		return match;
+	}
+
+	if (options?.inline) {
+		return `${match
+			.replace(/src=".*"/, "")
+			.replace(endTag, endTag === "/>" ? ">" : "")}{${JSON.stringify(
+			await res.text(),
+		)}}</script>`;
+	}
+
+	if (build.config?.outdir) {
+		const moduleFileName = new URL(res.url).pathname.split("/").pop() ?? "";
+
+		const savePath = `${build.config.outdir}${
+			build.config.publicPath ?? ""
+		}/js/${moduleFileName}`;
+
+		if (options?.verbose) console.log(`${pluginName}: Writing to ${savePath}`);
+
+		Bun.write(`${savePath}`, await res.text());
+		return `${match.replace(
+			url,
+			`${build.config?.publicPath ?? ""}/js/${moduleFileName}`,
+		)}`;
+	}
+
+	// If no outdir is provided or no inline option, leave it be
+	return match;
+};
+
+const handleModules = async (
+	build: PluginBuilder,
+	match: string,
+	groups: string[],
+	options?: Options,
+) => {
+	const [moduleName, endTag] = groups;
+	const modulefilePath = (() => {
+		try {
+			if (options?.verbose)
+				console.log(`${pluginName}: Resolving ${moduleName.split(":").pop()}`);
+			return import.meta.resolveSync(`${moduleName.split(":").pop()}`);
+		} catch (error) {}
+	})();
+
+	if (!modulefilePath) {
+		console.error(`${pluginName}: Cannot find module: ${moduleName}`);
+		return match;
+	}
+
+	const moduleFileContent = await Bun.file(modulefilePath).text();
+
+	if (options?.inline) {
+		return `${match
+			.replace(/src=".*"/, "")
+			.replace(endTag, endTag === "/>" ? ">" : "")}{${JSON.stringify(
+			moduleFileContent,
+		)}}</script>`;
+	}
+
+	if (build.config?.outdir) {
+		const moduleFileName = modulefilePath.split("/").pop() ?? "";
+
+		const savePath = `${build.config.outdir}${
+			build.config.publicPath ?? ""
+		}/js/${moduleFileName}`;
+
+		if (options?.verbose) console.log(`${pluginName}: Writing to ${savePath}`);
+
+		await Bun.write(savePath, moduleFileContent);
+		return `${match.replace(
+			moduleName,
+			`${build.config?.publicPath ?? ""}/js/${moduleFileName}`,
+		)}`;
+	}
+
+	// If no outdir is provided or no inline option, leave it be
+	return match;
+};
+
+/**
+ *
+ * @param options Options for the plugin.
+ * @param extras Provide extra data to the plugin.
+ * @returns {BunPlugin}
+ */
+const JSXScriptBundlerPlugin = (
+	options?: Options,
+	extras?: ExtraArgs,
+): BunPlugin & { onLoadCallback: typeof onLoadCallback } => {
+	const regexp =
+		/<script .*((?=npm:|http:\/\/|https:\/\/).*?)(?=").*(\/>|<\/script>)/g;
+
+	const onLoadCallback: (
+		build: PluginBuilder,
+		args: OnLoadArgs,
+	) => OnLoadResult | Promise<OnLoadResult> = async (build, { path }) => {
+		const contents = extras?.sourceFiles
+			? extras.sourceFiles[path]
+			: await Bun.file(path).text();
+		const matches = await Promise.all(
+			Array.from(contents.matchAll(regexp), async ([match, g1, endTag]) => {
+				if (g1.includes("http://") || g1.includes("https://")) {
+					return handleRemoteScripts(build, match, [g1, endTag], options);
+				}
+
+				return handleModules(build, match, [g1, endTag], options);
+			}),
+		);
+
+		let i = 0;
+
+		return {
+			contents: contents.replace(regexp, () => matches[i++]),
+		};
+	};
 
 	return {
-		name: "bun-plugin-jsx-script-bundle",
-		setup(builder) {
-			builder.onLoad({ filter: /\.(jsx|tsx)$/ }, async (args) => {
-				const contents = readFileSync(args.path, "utf-8");
-
-				const matches = await Promise.all(
-					Array.from(contents.matchAll(regexp), async ([match, g1]) => {
-						let fileName: string;
-
-						if (g1.includes("http://") || g1.includes("https://")) {
-							const urlPath: string = g1;
-
-							if (config?.verbose) console.log(`Downloading ${urlPath}`);
-
-							const res = await fetch(urlPath);
-
-							fileName = new URL(res.url).pathname.split("/").pop() ?? "";
-
-							if (builder.config.outdir) {
-								const savePath = `${builder.config.outdir ?? import.meta.dir}${
-									builder.config.publicPath ?? ""
-								}/js/${fileName}`;
-								if (res.ok) {
-									res.text().then((text) => {
-										if (config?.verbose) console.log(`Writing to ${savePath}`);
-
-										Bun.write(`${savePath}`, text);
-									});
-								} else {
-									console.error(`Failed to fetch ${urlPath}`);
-									return match;
-								}
-							}
-						} else {
-							const filePath = (() => {
-								try {
-									return import.meta.resolveSync(`${g1.split(":").pop()}`);
-								} catch (error) {}
-							})();
-
-							if (!filePath) {
-								console.error(`Cannot find module: ${g1}`);
-								return match;
-							}
-
-							fileName = filePath.split("/").pop() ?? "";
-
-							const file = readFileSync(filePath);
-
-							if (builder.config.outdir) {
-								const savePath = `${builder.config.outdir ?? import.meta.dir}${
-									builder.config.publicPath ?? ""
-								}/js/${fileName}`;
-
-								if (config?.verbose) console.log(`Writing to ${savePath}`);
-
-								Bun.write(savePath, file);
-							}
-						}
-
-						return `${match.replace(
-							g1,
-							`${builder.config.publicPath ?? ""}/js/${fileName}`,
-						)}`;
-					}),
-				);
-
-				let i = 0;
-
-				return {
-					contents: contents.replace(regexp, () => matches[i++]),
-					loader: args.loader,
-				};
-			});
+		name: pluginName,
+		setup(build) {
+			build.onLoad({ filter: /\.(jsx|tsx)$/ }, (args) =>
+				onLoadCallback(build, args),
+			);
 		},
+		onLoadCallback,
 	};
-}
-
+};
 
 export default JSXScriptBundlerPlugin;
